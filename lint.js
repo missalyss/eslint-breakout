147d3c295ea76389600460331978b1afb01c2fb9
DerekWW <derek.wierson@gmail.com>
2017-04-10 08:00:50 -0700

    create base breakout

---
+++ lint.js
@@ -0,0 +1,12 @@
+var fizzBuzz = (num) => {
+  if (num % 5 === 0 && num % 3 === 0) {
+  console.log('fizzBuzz');
+  }else if( num % 3 === 0 ){
+     console.log('fizz');
+  } else if(num%5 ===  0) {
+    console.log('buzz');
+  }
+
+
+
+};
